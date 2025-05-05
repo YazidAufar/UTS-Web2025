f1896b2f05f2a16b036c67443cc58fd133175357
YazidAufar <60860675+YazidAufar@users.noreply.github.com>
2025-05-05 08:54:49 +0800

    Add files via upload

---
+++ latihanmatematika.php
@@ -0,0 +1,23 @@
+<?php
+    $angka1 = 0.5; 
+    $angka2 = 1.5; 
+
+
+    // Penjumlahan
+    $penjumlahan = $angka1 + $angka2;
+    echo "<h2>Penjumlahan</h2>";
+    echo "<p>$angka1 + $angka2 = $penjumlahan</p>";
+    // Pengurangan
+    $pengurangan = $angka1 - $angka2;
+    echo "<h2>Pengurangan</h2>";
+    echo "<p>$angka1 - $angka2 = $pengurangan</p>";
+    // Perkalian
+    $perkalian = $angka1 * $angka2;
+    echo "<h2>Perkalian</h2>";
+    echo "<p>$angka1 * $angka2 = $perkalian</p>";
+    // Pembagian
+    $pembagian = $angka1 / $angka2;
+    echo "<h2>Pembagian</h2>";
+    echo "<p>$angka1 / $angka2 = $pembagian</p>";
+
+?>
